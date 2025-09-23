4c13eddc466a7695963c0098be30f26e0dd1c704
MarcDevGuy127 <marceloh.github@gmail.com>
2025-09-22 23:29:10 -0300

    Implementing do and while

--- jogo_adivinhe_o_numero/main.c
+++ jogo_adivinhe_o_numero/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+//caso windows32
+#ifdef _WIN32
+  #define CLEAR_CMD "cls"  // comando para limpar tela no Windows
+#else
+  #define CLEAR_CMD "clear" // comando para limpar tela em Unix-like
+#endif
+
+void limparTela() {
+    system(CLEAR_CMD);
+}
 
 int aleatoriza(int minimo, int maximo) {
     return minimo + rand() % (maximo - minimo + 1);
@@ -8,24 +20,39 @@ int aleatoriza(int minimo, int maximo) {
 
 int main()
 {
-    int num_digitado;
-
     srand(time(NULL));
 
-    //testando recebimento do valor digitado
-    printf("Adivinhe o numero que estou pensando!\n");
-    printf("Darei dicas quando necessario.\n");
+    int num_secreto = aleatoriza(1,100);
+    int num_digitado;
 
-    //aleatoriza(1, 100);
+    do {
+        printf("Adivinhe o numero que estou pensando!\n");
+        printf("Darei dicas quando necessario.\n");
+        printf("Digite um numero: ");
+        scanf("%d", &num_digitado);
+        getchar();
 
-    //testando numero aleatorio
-    printf("Numero Aleatorio: %d", aleatoriza(1, 100));
+        if(num_digitado < num_secreto) {
+            printf("Dica: o numero que estou pensando e maior que %d.", num_digitado);
 
-    //printf("Digite um numero: ");
-    //scanf("%d", &num_digitado);
-    //printf("Valor digitado: %d", num_digitado);
+            getchar();
 
-    //if(num_digitado == num_ad) {};
+            limparTela();
+        } else if(num_digitado > num_secreto) {
+
+            printf("Dica: o numero que estou pensando e menor que %d.", num_digitado);
+
+            getchar();
+
+            limparTela();
+        } else {
+            printf("Voce ganhou!");
+
+            getchar();
+
+            limparTela();
+        };
+    } while (num_digitado != num_secreto);
 
     return 0;
 }
