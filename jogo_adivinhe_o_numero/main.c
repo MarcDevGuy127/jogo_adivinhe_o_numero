bd68d50a1ff8a4eabed3033812bfff02f9a057b7
MarcDevGuy127 <marceloh.github@gmail.com>
2025-09-22 22:56:06 -0300

    Testing random values generator

--- jogo_adivinhe_o_numero/main.c
+++ jogo_adivinhe_o_numero/main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int aleatoriza(int minimo, int maximo) {
+    return minimo + rand() % (maximo - minimo + 1);
+}
 
 int main()
 {
     int num_digitado;
 
+    srand(time(NULL));
+
     //testando recebimento do valor digitado
     printf("Adivinhe o numero que estou pensando!\n");
     printf("Darei dicas quando necessario.\n");
 
-    printf("Digite um numero: ");
-    scanf("%d", &num_digitado);
-    printf("Valor digitado: %d", num_digitado);
+    //aleatoriza(1, 100);
+
+    //testando numero aleatorio
+    printf("Numero Aleatorio: %d", aleatoriza(1, 100));
+
+    //printf("Digite um numero: ");
+    //scanf("%d", &num_digitado);
+    //printf("Valor digitado: %d", num_digitado);
+
+    //if(num_digitado == num_ad) {};
 
     return 0;
 }
